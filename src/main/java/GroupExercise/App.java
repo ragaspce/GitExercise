3c61ee7cb51dde2cf1c1ee7dd42d86185856e90f
ragaspce <ragaspce@gmail.com>
2019-10-14 15:35:40 +0530

    v 1.0 src file Dev

---
+++ src/main/java/GroupExercise/App.java
@@ -0,0 +1,13 @@
+package GroupExercise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
